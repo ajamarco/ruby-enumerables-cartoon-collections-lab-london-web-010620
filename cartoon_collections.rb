ac3811fcde579584958cf0b008b0a2d7fa60b2e1
Alexandre Jamarco <alexandre.j@gmail.com>
2019-12-17 16:34:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves_array)
-  dwarves_array.each_with_index{|item, index| puts "#{index}. #{item}"
+  dwarves_array.each_with_index{|item, index| puts "#{index + 1}. #{item}"
   }
 end
 
